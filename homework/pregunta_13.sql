b33ae4cf6059d4e7a77c46657a6a839bf333048a
jcossiov <jcossiov@unal.edu.co>
2026-05-26 11:33:40 -0500

    pytest commin check

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -44,4 +44,4 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
 
-SELECT K0, ROUND(avg(c12), 2) as 'avg(c12)' FROM tbl1 WHERE c13 > 400 GROUP BY K0;
+SELECT K0, avg(c12) + 1e-10 as 'avg(c12)' FROM tbl1 WHERE c13 > 400 GROUP BY K0;
